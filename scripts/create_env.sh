630b98d698dd3f78d234eee3f1b2301a7feacc4c
bricksdont <mathias.mueller@uzh.ch>
2025-07-07 13:51:59 +0200

    create env

---
+++ scripts/create_env.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+module load gpu cuda/12.6.2 cudnn/9.5.1.17-12 anaconda3
+
+scripts=$(dirname "$0")
+base=$scripts/../..
+
+venvs=$base/venvs
+
+# perhaps not necessary anymore
+# export TMPDIR="/var/tmp"
+
+mkdir -p $venvs
+
+# venv for HF
+
+conda create -y --prefix $venvs/huggingface python=3.9.6
