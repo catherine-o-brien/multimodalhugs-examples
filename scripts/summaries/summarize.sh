2fc9e1ce7bd51503a987c470057ee5439267cdc2
bricksdont <mathias.mueller@uzh.ch>
2025-10-10 14:14:59 +0200

    summary

--- scripts/summaries/summarize.sh
+++ scripts/summaries/summarize.sh
@@ -10,4 +10,9 @@ summaries=$base/summaries
 
 mkdir -p $summaries
 
-python3 $scripts/summaries/summarize.py --eval-folder $evaluations > $summaries/summary.tsv
+# python3 $scripts/summaries/summarize.py --eval-folder $evaluations > $summaries/summary.tsv
+
+grep "\"score\"" $evaluations/*/test_score.bleu | awk -F'"score": ' '{print $2 "\t" $0}' | sort -k1,1nr | cut -f2- \
+    > $summaries/summary.txt
+
+cat $summaries/summary.txt
